bbcb7350d1d2126cd9fe86e4bd05b4df584cd16d
@Maival1 <maival.cimba@gmail.com>
2023-12-10 20:14:08 +0100

    feat(server api) get and post news, add static

--- server/controllers/newsControllers.js
+++ server/controllers/newsControllers.js
@@ -1,15 +1,26 @@
 const { News } = require('../models/models')
 const ApiError = require('../error/ApiError')
+const uuid = require('uuid')
+const path = require('path')
 
 class NewsController {
-  async create(req, res) {
-    const {title, text, img, description} = req.body
-    const news = await News.create({title, text, img, description})
-    return res.json(news)
+  async create(req, res, next) {
+    try {
+      const {title, text, description} = req.body
+      const {img} = req.files
+      const fileName = uuid.v4() + '.jpg'
+      img.mv(path.resolve(__dirname, '..', 'static', fileName))
+      const news = await News.create({title, text, description, img: fileName})
+      return res.json(news)
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
+
   }
 
   async getAll(req, res) {
-
+    const newsAll = await News.findAll()
+    return res.json(newsAll)
   }
 
   async getOne(req, res) {
