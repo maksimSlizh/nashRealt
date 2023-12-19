ca011caa1b9079cdd4f960168ad24cd4516688c3
@Maival1 <maival.cimba@gmail.com>
2023-12-19 15:01:38 +0100

    feat(fix routers)

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,9 +1,24 @@
-import {Switch, Route, Redirect} from 'react-router-dom'
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../router/router';
+import { PREW_ROUTE } from '../utils/consts';
 
 export function AppRouter() {
-  return (
-    <>
+  const isAuth = false;
 
-    </>
-  )
+  return (
+    <Routes>
+      {/* Маршруты для авторизованных пользователей */}
+      {isAuth &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+
+      {/* Маршруты для неавторизованных пользователей */}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+      <Route path="*" element={<Navigate to={PREW_ROUTE} />} />
+    </Routes>
+  );
 }
